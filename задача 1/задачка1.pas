818b71c6ea032307ea84034491090a7d5c8229ab
kotsibirsky <rpatuk@mail.ru>
2017-03-07 20:09:06 +0300

    Добавил задачу 1

---
+++ задача 1/задачка1.pas
@@ -0,0 +1,9 @@
+var
+  a,b: integer;
+begin
+  writeln('¬ведите числа',a,b);
+  readln(a,b);
+  if a>b then writeln('a больше');
+  if a<b then writeln('b больше')
+    else writeln('числа равны');
+end.
